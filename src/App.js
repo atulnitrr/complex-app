291dd2e630d647db2ebd27d80cdcc05e97a66df0
Atul Kumar <atul.kumar3@go-mmt.com>
2020-05-06 09:21:55 +0530

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import About from "./components/About";
+import Terms from "./components/Terms";
 import HomeGuest from "./components/HomeGuest";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <HomeGuest />
+      <Switch>
+        <Route path="/" exact>
+          <HomeGuest />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/terms">
+          <Terms />
+        </Route>
+      </Switch>
       <Footer />
       {/* Container ends here */}
-    </>
+    </BrowserRouter>
   );
 }
 
