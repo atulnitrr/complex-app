bd10a48e2c601624aa14ddd02ff0c83e77f32e98
Atul Kumar <atul.kumar3@go-mmt.com>
2020-05-11 08:15:06 +0530

    Added delete button

--- src/components/ViewSinglePost.js
+++ src/components/ViewSinglePost.js
@@ -1,25 +1,24 @@
 import React, { useState, useContext, useEffect } from "react";
 import Axios from "axios";
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, withRouter } from "react-router-dom";
 import Page from "./Page";
 import LoadingIcon from "./LoadingIcon";
 import NotFound from "./NotFound";
 import ReactMarkdown from "react-markdown";
 import ReactToolTip from "react-tooltip";
+import StateContext from "../StateContext";
 
-export default function ViewSinglePost() {
+function ViewSinglePost(props) {
+  const appState = useContext(StateContext);
   const [isLoading, setIsloaig] = useState(true);
   const [post, setPost] = useState();
-  const id = useParams();
+  const { id } = useParams();
   useEffect(() => {
     const ourRequest = Axios.CancelToken.source();
     async function fetchPostData() {
-      console.log(id);
-      console.log(id.id);
-      const response = await Axios.get(`/post/${id.id}`, {
+      const response = await Axios.get(`/post/${id}`, {
         cancelToken: ourRequest.token,
       });
-      console.log(response.data);
       setPost(response.data);
       setIsloaig(false);
     }
@@ -43,29 +42,61 @@ export default function ViewSinglePost() {
     date.getMonth() + 1
   }/${date.getDate()}/${date.getFullYear()}`;
 
+  const isOwner = () => {
+    if (appState.loggedIn) {
+      return appState.user.username == post.author.username;
+    } else {
+      return false;
+    }
+  };
+
+  const deleteHandler = async (e) => {
+    const areYouSure = window.confirm("Do you want to delete");
+    if (areYouSure) {
+      try {
+        const response = await Axios.delete(`/post/${id}`, {
+          data: {
+            token: appState.user.token,
+          },
+        });
+        if (response.data === "Success") {
+          // display a flash message
+          // redirect to current user
+          props.history.push(`/profile/${appState.user.username}`);
+        }
+      } catch (error) {
+        console.log("There was an erro");
+      }
+    } else {
+    }
+  };
+
   return (
     <Page title={post.title}>
       <div className="d-flex justify-content-between">
         <h2>{post.title}</h2>
-        <span className="pt-2">
-          <Link
-            to={`/post/${post._id}/edit`}
-            data-tip="Edit"
-            data-for="edit"
-            className="text-primary mr-2"
-          >
-            <i className="fas fa-edit"></i>
-          </Link>
-          <ReactToolTip id="edit" className="custom-tooltip" />{" "}
-          <a
-            data-tip="Delete"
-            data-for="delete"
-            className="delete-post-button text-danger"
-          >
-            <i className="fas fa-trash"></i>
-          </a>
-          <ReactToolTip id="delete" className="custom-tooltip" />
-        </span>
+        {isOwner() && (
+          <span className="pt-2">
+            <Link
+              to={`/post/${post._id}/edit`}
+              data-tip="Edit"
+              data-for="edit"
+              className="text-primary mr-2"
+            >
+              <i className="fas fa-edit"></i>
+            </Link>
+            <ReactToolTip id="edit" className="custom-tooltip" />{" "}
+            <a
+              data-tip="Delete"
+              data-for="delete"
+              className="delete-post-button text-danger"
+              onClick={deleteHandler}
+            >
+              <i className="fas fa-trash"></i>
+            </a>
+            <ReactToolTip id="delete" className="custom-tooltip" />
+          </span>
+        )}
       </div>
 
       <p className="text-muted small mb-4">
@@ -88,3 +119,5 @@ export default function ViewSinglePost() {
     </Page>
   );
 }
+
+export default withRouter(ViewSinglePost);
